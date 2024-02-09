1cc128ed966a89946fb8ddd9419b5b087d73d372
Rafael-Alexandre-96 <rafael.alexandre.1996.ra@gmail.com>
2024-02-09 17:35:52 -0300

    lib: adicionando lib para valor extenso de real

--- src/service/utils/stringFormater.js
+++ src/service/utils/stringFormater.js
@@ -1,4 +1,6 @@
-export const formatData = (mongoDateTime) => {
+import { estilo, porExtenso } from 'numero-por-extenso';
+
+export const formatDateTime = (mongoDateTime) => {
     let year = mongoDateTime?.substring(0, 4);
     let month = mongoDateTime?.substring(5, 7);
     let day = mongoDateTime?.substring(8, 10);
@@ -6,6 +8,21 @@ export const formatData = (mongoDateTime) => {
     return `${day}/${month}/${year} ${hs}`
 };
 
+export const formatDate = (mongoDateTime) => {
+    let year = mongoDateTime?.substring(0, 4);
+    let month = mongoDateTime?.substring(5, 7);
+    let day = mongoDateTime?.substring(8, 10);
+    return `${day}/${month}/${year}`
+};
+
 export const limitString = (string, letters = 1) => {
     return string?.substring(0, letters);
-}
+};
+
+export const formatReal = (string) => {
+    return string?.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+};
+
+export const formatExtenso = (string) => {
+    return porExtenso(parseFloat(string?.toString()).toFixed(2), estilo.monetario);
+};
